b69f8f8c8b76cccb5ee3dd20d727e6b415728fad
fogi89188 <viktorpetrov02@abv.bg>
2021-04-18 18:24:49 +0300

    Adding Database Project

---
+++ Database/PublishProfiles/dbo.AccountTable.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[AccountTable] (
+    [Id]        INT            IDENTITY (1, 1) NOT NULL,
+    [FirstName] NVARCHAR (50)  NOT NULL,
+    [LastName]  NVARCHAR (50)  NOT NULL,
+    [Email]     NVARCHAR (100) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
