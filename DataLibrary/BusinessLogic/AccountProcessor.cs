ec6215e7e389b7fd1aabe5abe6329ba8e788f619
fogi89188 <viktorpetrov02@abv.bg>
2021-04-18 18:25:10 +0300

    Adding Data Library Project

---
+++ DataLibrary/BusinessLogic/AccountProcessor.cs
@@ -0,0 +1,34 @@
+﻿using DataLibrary.DataAccess;
+using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.BusinessLogic
+{
+    public static class AccountProcessor
+    {
+        public static int CreateAccount(string firstName, string lastName, string email)
+        {
+            AccountModel data = new AccountModel
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Email = email
+            };
+
+            string sql = @"insert into dbo.AccountTable (FirstName, LastName, Email) values (@FirstName, @LastName, @Email)";
+
+            return SqlDataAccess.SaveData(sql, data);
+        }
+
+        public static List<AccountModel> LoadAccounts()
+        {
+            string sql = @"select Id, FirstName, LastName, Email from dbo.AccountTable";
+
+            return SqlDataAccess.LoadData<AccountModel>(sql);
+        }
+    }
+}
---
+++ DataLibrary/Models/AccountModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Models
+{
+    public class AccountModel
+    {
+
+        #region Private
+        private string email;
+        private string lastName;
+        private string firstName;
+        private int id;
+        #endregion
+
+        #region Public
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        #endregion
+
+    }
+}
